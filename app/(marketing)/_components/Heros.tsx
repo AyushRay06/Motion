d8d2629a8892c1ba05a25debf6be4386fd284db7
AyushRay06 <rayayush47@gmail.com>
2024-07-15 22:20:10 +0530

    Landing page

---
+++ app/(marketing)/_components/Heros.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+
+export const Heros = () => {
+  return (
+    <div className="flex flex-col item-center justifify-center max-w-5xl">
+      <div className="flex items-center">
+        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+          <Image src="/banner.png" fill className="object-contain" alt="Hero" />
+        </div>
+        <div> </div>
+      </div>
+    </div>
+  )
+}
