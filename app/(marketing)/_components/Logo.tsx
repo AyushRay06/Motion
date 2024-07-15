65551b5d35c44a1e78926fd185392b6a53ca9125
AyushRay06 <rayayush47@gmail.com>
2024-07-16 02:29:19 +0530

    NavBar

--- app/(marketing)/_components/Logo.tsx
+++ app/(marketing)/_components/Logo.tsx
@@ -11,7 +11,7 @@ const font = Poppins({
 export const Logo = () => {
   return (
     <div className="hidden md:flex items-center gap-x-2">
-      <Image src="/logo.svg" height="40" width="40" alt="Logo" />
+      <Image src="/logo2.svg" height="40" width="40" alt="Logo" />
       <p className="{cn(" font-semibold>
         Motion
       </p>
