ac3a8da99a3a9b05f7e32862a0abe597e3c51199
AyushRay06 <rayayush47@gmail.com>
2024-07-16 14:09:27 +0530

    Dark-mode

--- app/(marketing)/_components/Heros.tsx
+++ app/(marketing)/_components/Heros.tsx
@@ -5,13 +5,30 @@ export const Heros = () => {
     <div className="flex flex-col item-center justifify-center max-w-5xl">
       <div className="flex items-center">
         <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
-          <Image src="/banner.jpg" fill className="object-contain" alt="Hero" />
+          <Image
+            src="/banner.jpg"
+            fill
+            className="object-contain dark:hidden"
+            alt="Hero"
+          />
+          <Image
+            src="/black.jpg"
+            fill
+            className="object-contain hidden dark:block"
+            alt="Hero"
+          />
         </div>
         <div className="relative h-[400px] w-[400px] hidden md:block">
           <Image
             src="/banner2.jpg"
             fill
-            className="object-contain"
+            className="object-contain dark:hidden"
+            alt="banner2"
+          />
+          <Image
+            src="/black.jpg"
+            fill
+            className="object-contain hidden dark:block"
             alt="banner2"
           />
         </div>
